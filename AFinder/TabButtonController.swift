6bf654fe31fe8fe67a1b1052869523e24d43bb63
Kutan <cingisiz.akutan@gmail.com>
2017-03-22 16:06:42 +0300

    a

---
+++ AFinder/TabButtonController.swift
@@ -0,0 +1,21 @@
+//
+//  TabButtonController.swift
+//  AFinder
+//
+//  Created by Kutan on 22/03/2017.
+//  Copyright © 2017 Argeka. All rights reserved.
+//
+
+import UIKit
+
+class TabButtonController: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
