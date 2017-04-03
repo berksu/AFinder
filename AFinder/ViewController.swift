fa0bc35e207c05b628de8836d8f12e110d0b6d90
Kutan <cingisiz.akutan@gmail.com>
2017-04-04 00:11:52 +0300

    Home logo button implemented

--- AFinder/ViewController.swift
+++ AFinder/ViewController.swift
@@ -17,24 +17,33 @@ class ViewController: UIViewController {
     
     var homeViewController : UIViewController!
     var findViewController : UIViewController!
+    var addProductController : UIViewController!
     
     var viewControllers : [UIViewController]!
     @IBOutlet var buttonDividers : [UIImageView]!
     var buttonDivierImages : [UIImage]!
     var selectedIndex : Int = 0
     
-    //berksu and kutan and berksu
+    @IBOutlet weak var newAdImageView: UIImageView!
+    let recognizer = UITapGestureRecognizer()
+    
+    var previousIndex : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        newAdImageView.isUserInteractionEnabled = true
+        recognizer.addTarget(self, action: #selector(ViewController.profileImageHasBeenTapped))
+        
+        newAdImageView.addGestureRecognizer(recognizer)
+        
         let storyboard =  UIStoryboard(name: "Main", bundle: nil)
         
         homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeView")
-        
         findViewController = storyboard.instantiateViewController(withIdentifier: "FindView")
+        addProductController = storyboard.instantiateViewController(withIdentifier: "ProductView")
         
-        viewControllers = [homeViewController,findViewController]
+        viewControllers = [homeViewController,findViewController,addProductController]
         
         let image : UIImage = UIImage(named:"divider_red")!
         let image_white : UIImage = UIImage(named:"divider_black")!
@@ -51,10 +60,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func profileImageHasBeenTapped(){
+        print("asd")
+        let previousVC = viewControllers[selectedIndex]
+        
+        previousVC.willMove(toParentViewController: nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        
+        //sender.isSelected = true
+        let vc = viewControllers[2]
+        
+        
+        //buttonDividers[selectedIndex].image = buttonDivierImages[0]
+        //buttonDividers[previousIndex].image = buttonDivierImages[1]
+        
+        addChildViewController(vc)
+        
+        vc.view.frame = contentView.bounds
+        contentView.addSubview(vc.view)
+        
+        vc.didMove(toParentViewController: self)
+        
+
+    }
 
     
     @IBAction func didPressTab(_ sender: UIButton) {
-        let previousIndex = selectedIndex
+        previousIndex = selectedIndex
         selectedIndex = sender.tag
         buttons[selectedIndex].isSelected = false
         
@@ -68,8 +102,8 @@ class ViewController: UIViewController {
         let vc = viewControllers[selectedIndex]
         
         
-        buttonDividers[selectedIndex].image = buttonDivierImages[0]
-        buttonDividers[previousIndex].image = buttonDivierImages[1]
+        //buttonDividers[selectedIndex].image = buttonDivierImages[0]
+        //buttonDividers[previousIndex].image = buttonDivierImages[1]
         
         addChildViewController(vc)
         
