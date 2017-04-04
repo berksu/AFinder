3ff065666beac7b1cf9214550a0a4c1e411c8dfa
Berksu <berksukismet@gmail.com>
2017-04-04 03:46:10 +0300

    visionApi

--- AFinder/ImageViewController.swift
+++ AFinder/ImageViewController.swift
@@ -33,7 +33,7 @@ class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UI
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated.a
     }
     
 
